0734d51b465c5a87fb55b1f695bf5d867a88aed1
Nikita1607 <75262824+Nikita1607@users.noreply.github.com>
2020-12-07 21:52:43 +0300

    init: new branch

---
+++ daterReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace daterReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

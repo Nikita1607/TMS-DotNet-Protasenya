d9c7eaf952bbcb06318049a7b288a367e5012f09
Nikita1607 <75262824+Nikita1607@users.noreply.github.com>
2020-12-10 17:28:52 +0300

    init: homework 2 (dateReader)

--- daterReader/Program.cs
+++ daterReader/Program.cs
@@ -6,7 +6,20 @@ namespace daterReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите дату: ");
+            string userInput = Console.ReadLine();
+            bool result = DateTime.TryParse(userInput, out DateTime date);
+            if (result )
+            {
+                Console.WriteLine(date.DayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }

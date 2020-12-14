e773f19a318a438da50a98ab16189e403073a49a
Nikita1607 <75262824+Nikita1607@users.noreply.github.com>
2020-12-14 18:58:16 +0300

    init:new HW,datereaderMetod

---
+++ HW3-datereaderMetod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW3_datereaderMetod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите дату: ");
+            string userInput = Console.ReadLine();
+            bool result = DateTime.TryParse(userInput, out DateTime date);
+            if (result)
+            {
+                Console.WriteLine(date.DayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
